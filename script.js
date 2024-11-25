9982556d3c33ecedc6d63adc88b34b8f4ecd19e3
Debasishsahu113 <debasishsahu405@gmail.com>
2024-11-25 23:44:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+let cityInput = document.getElementById("city")
+let searchBtn = document.getElementById("search-Btn")
+searchBtn.addEventListener("click", () => {
+    let city = cityInput.value.trim();
+    if (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=244c5b3edd2bbdedb7411837ec52c8d7&units=metric`)
+        .then(response => response.json())
+            .then(data => {
+                if (data.message) {
+                    alert(data.message)
+                }
+                else {
+                    // console.log(data.name);
+                    // console.log(data.main.temp);
+                    // console.log(data.main.humidity);
+                    // console.log(data.weather[0].main);
+                    document.querySelector(".info").style.display = "flex";
+                    document.getElementById("cityname").innerHTML = data.name;
+                    document.getElementById("temp").innerHTML ="Temp:-"+data.main.temp + "℃";
+                    document.getElementById("wind").innerHTML="WIND:-"+data.wind.speed + "m/s";
+                    document.getElementById("humidity").innerHTML=" HUMIDITY:-"+data.main.humidity + "%";
+                }
+            })
+            .catch(error => console.log(error))
+    } else {
+        alert("First write the city name")
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let url="https://api.openweathermap.org/data/2.5/weather?q=bengaluru&appid=244c5b3edd2bbdedb7411837ec52c8d7&units=metric"
+// let url="https://api.openweathermap.org/data/2.5/weather?q=bengaluru&appid=244c5b3edd2bbdedb7411837ec52c8d7"
+
+// let cityInput=document.getElementById("city")
+// let searchBtn=document.getElementById("search-Btn")
+
+// searchBtn.addEventListner("click",() =>{
+//     let city =cityInput.ariaValueMax.trim()
+//     if (city){
+//       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=244c5b3edd2bbdedb7411837ec52c8d7`)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error))
+//     }
+
+//     else{
+//         alert("First Write The City a Name")
+//     }
+// })
+// getdata(){
+//     console.log(data);
+    
+// }
+
